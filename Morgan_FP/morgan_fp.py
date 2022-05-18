268f5fa4baea136b6f98e0f33e4db951494878cf
pipitoludovico <67153855+pipitoludovico@users.noreply.github.com>
2022-05-18 20:49:14 +0100

    Update morgan_fp.py
    Now it creates the appropriate folder and cleans the mother folder :)

--- Morgan_FP/morgan_fp.py
+++ Morgan_FP/morgan_fp.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -11,10 +12,14 @@ def main():
     dfs = parser.get_df()
     count = 0
     for x in dfs:
+        print(x)
         fp_sample = Read_fingerprint(sys.argv[2])
         pattern = fp_sample.get_smile()
         finder = Finder(pattern, x, count)
         count += 1
+    os.system(f"mkdir {sys.argv[2].replace('.pdb','')}")
+    os.system(f"mv *.xlsx {sys.argv[2].replace('.pdb','')}")
+    os.system(f"mv {sys.argv[2].replace('.pdb','')}.* {sys.argv[2].replace('.pdb','')}")
 
 
 if __name__ == '__main__':
