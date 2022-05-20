810d599aeb5035ad86ebdf47507d157209ab552b
pipitoludovico <67153855+pipitoludovico@users.noreply.github.com>
2022-05-20 09:42:49 +0100

    Update read_db_2.py
    improved readability, removed useless variable, included duplicate drop.

--- Morgan_FP/fingerprint_finder/read_db_2.py
+++ Morgan_FP/fingerprint_finder/read_db_2.py
@@ -4,15 +4,15 @@ import numpy as np
 
 class Read_db_2:
     def __init__(self, file):
-        self.file = pd.read_csv(file, delimiter=";", chunksize=100000, low_memory=False, error_bad_lines=False)
+        self.file = pd.read_csv(file, delimiter=";", chunksize=1000000, low_memory=False, error_bad_lines=False)
         self.chunks = []
-        self.count = 0
         for x in self.file:
             x.replace(to_replace='None', value=np.nan, inplace=True)
             x.dropna(how="any", inplace=True)
             self.selection = x[['ChEMBL ID', 'Molecular Weight', 'HBA', 'HBD', 'Smiles']]
             self.selection['ChEMBL ID'] = self.selection['ChEMBL ID'].astype(str)
             self.selection['Smiles'] = self.selection['Smiles'].astype(str)
+            self.selection.drop_duplicates()
             self.filtered = self.selection[
                 (self.selection['Molecular Weight'] >= 120) & (self.selection['Molecular Weight'] <= 500) & (
                         self.selection['HBA'].astype(int) >= 2) & (self.selection['HBD'].astype(int) >= 1)]
